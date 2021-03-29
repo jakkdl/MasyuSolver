f5ad22c7d67cd01e0534a6343b03841d6896640d
dbt <dianne_taft@yahoo.com>
2021-03-29 10:41:54 -0700

    Added Comments

--- Cell.py
+++ Cell.py
@@ -1,63 +1,100 @@
+# This is a private class, used only by the PuzzleBoard class.
+#
 class Cell():
     TYPE_DOT = 0
     TYPE_BLACK_CIRCLE = 1
     TYPE_WHITE_CIRCLE = 2
 
+    # Constructor with default type of 'TYPE_DOT'
+    #
     def __init__(self, type = TYPE_DOT):
         self.type = type
         self.isCellEnabled = True
         self.isCellValid = True
 
+    # Returns an exact copy of the cell
+    #
     def clone(self):
         c = Cell(self.type)
         c.isCellEnabled = self.isCellEnabled
         c.isCellValid = self.isCellValid
         return(c)
 
+    # Resets the cell to: DOT, Valid, and Enabled
+    #
     def reset(self):
         self.type = Cell.TYPE_DOT
         self.isCellEnabled = True
         self.isCellValid = True
 
+    # Returns 'true' if cell is a BlACK CIRCLE
+    #
     def isBlackCircle(self):
         return(self.type == Cell.TYPE_BLACK_CIRCLE)
 
+    # Returns 'true' if cell is a WHITE CIRCLE
+    #
     def isWhiteCircle(self):
         return(self.type == Cell.TYPE_WHITE_CIRCLE)
 
+    # Returns 'true' if cell is a DOT
+    #
     def isDot(self):
         return(self.type == Cell.TYPE_DOT)
 
+    # Sets the cell to a BLACK CIRCLE
+    #
     def setAsBlackCircle(self):
         self.type = Cell.TYPE_BLACK_CIRCLE
 
+    # Sets the cell to a WHITE CIRCLE
+    #
     def setAsWhiteCircle(self):
         self.type = Cell.TYPE_WHITE_CIRCLE
 
+    # Sets the cell to a DOT
+    #
     def setAsDot(self):
         self.type = Cell.TYPE_DOT
 
+    # Returns 'true' if cell is Enabled
+    #
     def isEnabled(self):
         return(self.isCellEnabled)
 
+    # Marks the cell as Enabled
+    #
     def setEnabled(self):
         self.isCellEnabled = True
 
+    # Marks the cell as Disabled
+    #
     def setDisabled(self):
         self.isCellEnabled = False
 
+    # Returns 'true' if the cell is Valid
+    #
     def isValid(self):
         return(self.isCellValid)
 
+    # Marks the cell as Valid
+    #
     def setValid(self):
         self.isCellValid = True
 
+    # Marks the cell as Invalid
+    #
     def setInvalid(self):
         self.isCellValid = False
 
+    # Returns the cell type
+    #
     def getType(self):
         return(self.type)
 
+    # Prints the cell type: 'D' for DOT, 'B' for BLACK CIRCLE, 'W' for WHITE CIRCLE,
+    #   or '?' for anything else
+    #
     def print(self):
         if self.type == Cell.TYPE_DOT:
             print("D", end = "")
