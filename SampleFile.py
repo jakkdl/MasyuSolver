94a1dc7de204365e67a84de1d93015534ec7cad6
fdt <fred.taft@yahoo.com>
2021-04-01 19:31:21 -0700

    More experimenting with merging

--- SampleFile.py
+++
@@ -1,8 +0,0 @@
-print ("This is a Sample File")
-
-print("This statement was added by gwt ")
-
-if __name__ == "__main__":
-    print("SampleFile.py is running as the main code")
-print("this is aaron's change")
-print("this is dianne's change")
