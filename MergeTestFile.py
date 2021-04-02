45f1e324005e9e75a559c23fed68ff46fd9b4918
fdt <fred.taft@yahoo.com>
2021-04-02 03:58:19 -0700

    Adding a test file to help experiment with file merging using the source control system.

---
+++ MergeTestFile.py
@@ -0,0 +1,15 @@
+# This is a test file for how merging works
+
+def workArea1():
+    print("Entering workArea1()")
+    print("\tFred's initial work in workArea1()")
+    print("Leaving workArea1()")
+
+def workArea2():
+    print("Entering workArea2()")
+    print("\tFred's initial work in workArea2()")
+    print("Leaving workArea2()")
+
+if __name__ == "__main__":
+    workArea1()
+    workArea2()
