8be91205931097ad6ca58ca1a86792ff7d4501ff
fdt <fred.taft@yahoo.com>
2021-04-02 06:33:58 -0700

    Another merge test

--- MergeTestFile.py
+++ MergeTestFile.py
@@ -25,3 +25,4 @@ if __name__ == "__main__":
     # This is probably a conflicting merge .. will have to be manually dealt with
     nonConflictingMerge()
     workArea3()  # Conflicting change
+    # Just adding a comment
