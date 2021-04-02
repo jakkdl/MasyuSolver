0a6ca4549cc0810e12d96174d056e8ea1869c9ba
fdt <fred.taft@yahoo.com>
2021-04-02 04:08:18 -0700

    Reverting to previous version of merge file

--- MergeTestFile.py
+++ MergeTestFile.py
@@ -10,12 +10,6 @@ def workArea2():
     print("\tFred's initial work in workArea2()")
     print("Leaving workArea2()")
 
-def workArea3():
-    print("Entering workArea3()")
-    print("\tFred's initial work in workArea3()")
-    print("Leaving workArea3()")
-
 if __name__ == "__main__":
     workArea1()
     workArea2()
-    workArea3()
