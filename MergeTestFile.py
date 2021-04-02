1af402b443299ea7652f0fdbf727ad7d96fe3271
dbt <dianne_taft@yahoo.com>
2021-04-02 06:07:42 -0700

    Trying to commit changes to a file which had also been changed first by someone else .. should require a merge

--- MergeTestFile.py
+++ MergeTestFile.py
@@ -10,6 +10,11 @@ def workArea2():
     print("\tFred's initial work in workArea2()")
     print("Leaving workArea2()")
 
+def nonConflictingMerge():
+    print("This should be a non-conflicting merge!")
+
 if __name__ == "__main__":
     workArea1()
     workArea2()
+    # This is probably a conflicting merge .. will have to be manually dealt with
+    nonConflictingMerge()
