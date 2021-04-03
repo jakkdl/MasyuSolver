47652a56454b126e5eade96d8422fd79191632ee
aarontaft <taftenator@gmail.com>
2021-04-03 15:52:52 -0700

    implemented exception classes

---
+++ MasyuExceptions.py
@@ -0,0 +1,18 @@
+class MasyuException(Exception):
+    '''Base class for all solver exceptions'''
+
+    def __init__(self, msg):
+        self.msg = msg
+
+    def __str__(self):
+        return(self.msg)
+
+class MasyuSolverException(MasyuException):
+    '''Exception encountered during the solving process'''
+
+    def __init__(self, msg, location):
+        super().__init__(msg)
+        self.location = location    # this is a tuple
+
+    def __str__(self):
+        return(self.msg + " : " + repr(self.location))
