6828611606f9de9cf2cda5dcfb5400ab4244a9e5
aarontaft <taftenator@gmail.com>
2021-04-08 16:03:50 -0700

    implemented black circle processing and dead end processing

--- Solver.py
+++ Solver.py
@@ -24,12 +24,100 @@ class Solver():
         # todo findPathwaysToBlock
 
     def __processDeadendPaths(self, puzzleBoard):
-        print("__processDeadendPaths")
-        # todo __processDeadendPaths
+        numRows, numCols = puzzleBoard.getDimensions()
+        changesMade = False
+        deadEndFound = True
+        while deadEndFound:
+            deadEndFound = False
+            for rowNum in range(0, numRows):
+                for colNum in range(0, numCols):
+                    if (puzzleBoard.isDotAt(rowNum, colNum)):
+                        count, l, r, u, d = puzzleBoard.getBlockedPaths(rowNum, colNum)
+                        if (count == 3):
+                            numLines, lineLeft, lineRight, lineUp, lineDown = puzzleBoard.getLines(rowNum, colNum)
+                            if (numLines != 0):
+                                raise MasyuSolverException("Unexpected line at dead-end", (rowNum, colNum))
+                            else:
+                                if not (l):
+                                    deadEndFound = True
+                                    puzzleBoard.markBlockedLeft(rowNum, colNum)
+                                elif not (r):
+                                    deadEndFound = True
+                                    puzzleBoard.markBlockedRight(rowNum, colNum)
+                                elif not (u):
+                                    deadEndFound = True
+                                    puzzleBoard.markBlockedUp(rowNum, colNum)
+                                elif not (d):
+                                    deadEndFound = True
+                                    puzzleBoard.markBlockedDown(rowNum, colNum)
+
+            changesMade = changesMade | deadEndFound
+
+        return (changesMade)
 
     def __processBlackCircles(self, puzzleBoard):
-        print("__processBlackCircles")
-        # todo __processBlackCircles
+        numRows, numCols = puzzleBoard.getDimensions()
+        changesMade = False
+        for rowNum in range(0, numRows):
+            for colNum in range(0, numCols):
+                if (puzzleBoard.isBlackCircleAt(rowNum, colNum)):
+                    if (puzzleBoard.isBlockedLeft(rowNum, colNum) or
+                        ((colNum > 0) and puzzleBoard.isBlockedLeft(rowNum, (colNum - 1)))):
+                        if (colNum > (numCols - 3)) or \
+                                puzzleBoard.isBlockedRight(rowNum, colNum) or \
+                                puzzleBoard.isBlockedRight(rowNum, (colNum + 1)):
+                            raise MasyuSolverException("Black Circle Blocked L/R", (rowNum, colNum))
+                        else:
+                            if not (puzzleBoard.hasLineRight(rowNum, colNum)):
+                                changesMade = True
+                                puzzleBoard.drawLineRight(rowNum, colNum)
+                            if not (puzzleBoard.hasLineRight(rowNum, (colNum + 1))):
+                                changesMade = True
+                                puzzleBoard.drawLineRight(rowNum, (colNum + 1))
+
+                    if (puzzleBoard.isBlockedRight(rowNum, colNum) or
+                        ((colNum < (numCols - 1) and puzzleBoard.isBlockedRight(rowNum, (colNum + 1))))):
+                        if ((colNum < 2) or \
+                                puzzleBoard.isBlockedLeft(rowNum, colNum) or \
+                                puzzleBoard.isBlockedLeft(rowNum, (colNum - 1))):
+                            raise MasyuSolverException("Black Circle Blocked L/R - 2", (rowNum, colNum))
+                        else:
+                            if not (puzzleBoard.hasLineLeft(rowNum, colNum)):
+                                changesMade = True
+                                puzzleBoard.drawLineLeft(rowNum, colNum)
+                            if not (puzzleBoard.hasLineLeft(rowNum, (colNum - 1))):
+                                changesMade = True
+                                puzzleBoard.drawLineLeft(rowNum, (colNum - 1))
+
+                    if (puzzleBoard.isBlockedUp(rowNum, colNum) or
+                        ((rowNum > 0) and puzzleBoard.isBlockedUp((rowNum - 1), colNum))):
+                        if (rowNum > (numRows - 3)) or \
+                                puzzleBoard.isBlockedDown(rowNum, colNum) or \
+                                puzzleBoard.isBlockedDown((rowNum + 1), colNum):
+                            raise MasyuSolverException("Black Circle Blocked U/D", (rowNum, colNum))
+                        else:
+                            if not (puzzleBoard.hasLineDown(rowNum, colNum)):
+                                changesMade = True
+                                puzzleBoard.drawLineDown(rowNum, colNum)
+                            if not (puzzleBoard.hasLineDown((rowNum + 1), colNum)):
+                                changesMade = True
+                                puzzleBoard.drawLineDown((rowNum + 1), colNum)
+
+                    if (puzzleBoard.isBlockedDown(rowNum, colNum) or
+                        ((rowNum < (numRows - 1)) and puzzleBoard.isBlockedDown((rowNum + 1), colNum))):
+                        if (rowNum < 2) or \
+                                puzzleBoard.isBlockedUp(rowNum, colNum) or \
+                                puzzleBoard.isBlockedUp((rowNum - 1), colNum):
+                            raise MasyuSolverException("Black Circle Blocked U/D - 2", (rowNum, colNum))
+                        else:
+                            if not (puzzleBoard.hasLineUp(rowNum, colNum)):
+                                changesMade = True
+                                puzzleBoard.drawLineUp(rowNum, colNum)
+                            if not (puzzleBoard.hasLineUp((rowNum - 1), colNum)):
+                                changesMade = True
+                                puzzleBoard.drawLineUp((rowNum - 1), colNum)
+
+        return (changesMade)
 
     def __processWhiteCircles(self, puzzleBoard):
         numRows, numCols = puzzleBoard.getDimensions()
